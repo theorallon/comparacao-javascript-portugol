1a7e9698cd7aea0350ced27e7da67aaf45d536da
theorallon <theo_rallon@estudante.sesisenai.org.br>
2025-09-11 19:31:44 -0300

    adições na commitr de variaveis

--- script.js
+++ script.js
@@ -4,9 +4,12 @@ let cidade = "Rio de Janeiro - RJ";
 let altura = 1.83;
 let ehHumano = true;
 
+console.log(typeof nome);
+
 console.log("Nome: ", nome);
 console.log("Idade: ", idade);
 console.log("Altura: ", altura);
 console.log("Cidade: ", cidade);
 console.log("É humano? ", ehHumano);
 
+console.log(`Meu nome é ${nome}, tenho ${idade}, minha altura é ${altura}, sou natural da cidade ${cidade}, sou um humano? ${ehHumano}`);
