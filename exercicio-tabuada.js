0d64c44223436e102d49f74e053ce73ee0a1bf79
theorallon <theo_rallon@estudante.sesisenai.org.br>
2025-09-15 20:55:12 -0300

    tabuada

---
+++ exercicio-tabuada.js
@@ -0,0 +1,8 @@
+let numero = 6;
+
+console.log(`=== Tabuada do ${numero} ===`)
+
+for (let i = 1; i <= 10; i++) {
+    let resultado = numero * i;
+    console.log(`${numero} x ${i} = ${resultado}`)
+}
