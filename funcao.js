c51e6777b8187c39250d53499b01d92e423c95f8
theorallon <theo_rallon@estudante.sesisenai.org.br>
2025-09-15 20:55:23 -0300

    funções (function)

---
+++ funcao.js
@@ -0,0 +1,46 @@
+/*
+
+    Função em portugol
+
+    funão real calculadoraMedia(real n1, real n2){
+        retorne (n1 + n2) / 2
+    }
+
+*/
+
+
+//média
+function calculadoraMedia(n1, n2) {
+    return (n1 + n2) / 2;
+}
+
+console.log(calculadoraMedia(8,7));
+
+
+//soma
+function soma(n1, n2) {
+    return (n1 + n2);
+}
+
+console.log(soma(8,7));
+
+//subtração
+function sub(n1, n2) {
+    return (n1 - n2);
+}
+
+console.log(sub(8,7)); 
+
+//multiplicação
+function mult(n1, n2) {
+    return (n1 * n2);
+}
+
+console.log(mult(8,7));
+
+//divisão
+function div(n1, n2) {
+    return (n1 / n2);
+}
+
+console.log(div(8,7));
